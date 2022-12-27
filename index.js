41cdab40ad87c31dc2e744715e61482f5529f2a6
Omer Khan <omerkhan.nu@gmail.com>
2022-12-27 15:31:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Task#1
+// Given a string x, return true if x is palindrome.
+// An string is a palindrome when it reads the same backward as forward.
+// For example, abcba is a palindrome while abbca is not.
+//acbba
+
+const { log } = require("console");
+
+
+/**
+ * @param {string} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+const pal= 'abcba';//rabcba
+let len = pal.length-1;
+  for(let i = len; i >= 0; i++){
+    return (i);
+    //rabcba
+
+    
+  }
+   for( let j=0; j<=pal; i++){
+      return (j)
+     //abcbar
+    }
+  if(j ==i){
+    log.console('Palindrom')
+  }
+  else {
+    log.console("is Not Palindrom")
+  }
+};
